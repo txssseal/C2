5036bcdcfc8745ce90f3cb12c507645cee0ff44b
Raphael Villas <raphael.villas@gmail.com>
2014-04-17 12:16:54 -0500

    *** WIP: Move total_price_from_params back into the controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,2 @@
 module ApplicationHelper
-  include CommunicartsHelper
 end
--- app/helpers/communicarts_helper.rb
+++ app/helpers/communicarts_helper.rb
@@ -1,9 +1,9 @@
 module CommunicartsHelper
-  def total_price_from_params(cart_items)
-    sum = cart_items.reduce(0) do |sum,value|
-      sum + (value["qty"].gsub(/[^\d\.]/, '').to_f *  value["price"].gsub(/[^\d\.]/, '').to_f)
-    end
+  # def total_price_from_params(cart_items)
+  #   sum = cart_items.reduce(0) do |sum,value|
+  #     sum + (value["qty"].gsub(/[^\d\.]/, '').to_f *  value["price"].gsub(/[^\d\.]/, '').to_f)
+  #   end
 
-    return sum
-  end
+  #   return sum
+  # end
 end
