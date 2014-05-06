0ccb2264a9733b995385038e57dd1b55fb75a743
Raphael Villas <raphael.villas@gmail.com>
2014-05-06 15:55:02 -0500

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505150000) do
+ActiveRecord::Schema.define(version: 20140506204115) do
 
   create_table "approval_groups", force: true do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20140505150000) do
   create_table "approver_comments", force: true do |t|
     t.text     "comment_text"
     t.integer  "approver_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+
+  create_table "approvals", force: true do |t|
+    t.integer  "cart_id"
+    t.integer  "user_id"
+    t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -69,6 +76,14 @@ ActiveRecord::Schema.define(version: 20140505150000) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "approval_group_id"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email_address"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
