4c56d8e7474bc315abb2a21154bd2f86e6063bc1
Raphy Villas <raphael.villas@gmail.com>
2014-12-12 18:00:13 -0500

    *** Load Mail Preview conditionally

--- lib/mail_preview.rb
+++ lib/mail_preview.rb
@@ -1,40 +1,42 @@
-class MailPreview < MailView
-  def cart_notification_email
-    CommunicartMailer.cart_notification_email(email, pending_approval)
-  end
+unless Rails.env.development?
+  class MailPreview < MailView
+    def cart_notification_email
+      CommunicartMailer.cart_notification_email(email, pending_approval)
+    end
 
-  def cart_observer_email
-    CommunicartMailer.cart_observer_email(email, cart)
-  end
+    def cart_observer_email
+      CommunicartMailer.cart_observer_email(email, cart)
+    end
 
-  def approval_reply_received_email
-    CommunicartMailer.approval_reply_received_email(received_approval)
-  end
+    def approval_reply_received_email
+      CommunicartMailer.approval_reply_received_email(received_approval)
+    end
 
-  def comment_added_email
-    CommunicartMailer.comment_added_email(comment, email)
-  end
+    def comment_added_email
+      CommunicartMailer.comment_added_email(comment, email)
+    end
 
 
-  private
+    private
 
-  def email
-    'recipient@example.com'
-  end
+    def email
+      'recipient@example.com'
+    end
 
-  def pending_approval
-    Approval.pending.last
-  end
+    def pending_approval
+      Approval.pending.last
+    end
 
-  def received_approval
-    Approval.received.last
-  end
+    def received_approval
+      Approval.received.last
+    end
 
-  def cart
-    Cart.last
-  end
+    def cart
+      Cart.last
+    end
 
-  def comment
-    Comment.where(commentable_type: 'Cart').last
+    def comment
+      Comment.where(commentable_type: 'Cart').last
+    end
   end
 end
